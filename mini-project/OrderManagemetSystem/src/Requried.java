b4f7530119d6da6d9ade5fe21d65e50a343e34cb
Suraj.S <surajsuri279@gmail.com>
2023-08-01 12:19:56 +0530

    updated with oops

--- mini-project/OrderManagemetSystem/src/Requried.java
+++ mini-project/OrderManagemetSystem/src/Requried.java
@@ -4,24 +4,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-public class Requried {
+public class Requried  implements  JdbcAbs{
 
-
-    public static  int newUserOrExsitingUser(){
+    public    int newUserOrExsitingUser(){
         Scanner sc=new Scanner(System.in);
         System.out.println("1.New User!");
         System.out.println("2.Exsisting User!");
-
         System.out.println("Select the option");
-        int newUserOrExsistinUser=sc.nextInt();
-        return newUserOrExsistinUser;
-
+        int newUserOExsistinUser=sc.nextInt();
+        return newUserOExsistinUser;
 
     }
 
-
-
-    public  static String[] newUser(){
+    public    String[] newUser(){
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter your name");
         String name=sc.nextLine();
@@ -33,7 +28,7 @@ public class Requried {
     }
 
 
-    public static int exsistingUser(){
+    public   int exsistingUser(){
         Scanner sc=new Scanner(System.in);
 
         System.out.println("1.View products");
@@ -52,64 +47,84 @@ public class Requried {
 
 
 
-    public static void productViewTable(ResultSet resultSet)  throws SQLException{
+    public   void productViewTable(ResultSet resultSet) {
 
-        while (resultSet.next()){
-            System.out.println(resultSet.getInt("productId")+" "+resultSet.getString("productName")+" "+resultSet.getInt("price"));
+        try{
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt("productId")+" "+resultSet.getString("productName")+" "+resultSet.getInt("price"));
+            }
         }
-
-
+        catch (Exception e){
+            System.out.println(e);
+        }
 
     }
 
-    public static void Order(ResultSet resultSet, Connection connection, String name) throws SQLException {
-        System.out.println("Enter the product name");
-        Scanner sc=new Scanner(System.in);
-        String  orderOption=sc.nextLine();
-        String sqlInsert = "insert into orders values(?,?,?)";
+    public   void Order(ResultSet resultSet, Connection connection, String name)  {
 
-        int count=0;
+        try{
+            System.out.println("Enter the product name");
+            Scanner sc=new Scanner(System.in);
+            String  orderOption=sc.nextLine();
+            String sqlInsert = "insert into orders values(?,?,?)";
 
-        int prodId;
+            int count=0;
+
+            int prodId;
 
 
 
-        int flag=0;
+            int flag=0;
 
-        while(resultSet.next()){
-            count++;
-            if(resultSet.getString(2).equals(orderOption)){
-                prodId=resultSet.getInt("productId");
-                PreparedStatement preparedStatement = connection.prepareStatement(sqlInsert);
-                preparedStatement.setInt(1, count + 1);
-                preparedStatement.setString(2,name);
-                preparedStatement.setInt(3, prodId);
+            while(resultSet.next()){
+                count++;
+                if(resultSet.getString(2).equals(orderOption)){
+                    prodId=resultSet.getInt("productId");
+                    PreparedStatement preparedStatement = connection.prepareStatement(sqlInsert);
+                    preparedStatement.setInt(1, count + 1);
+                    preparedStatement.setString(2,name);
+                    preparedStatement.setInt(3, prodId);
 
-                int rowsInserted = preparedStatement.executeUpdate();
+                    int rowsInserted = preparedStatement.executeUpdate();
 
-                if(rowsInserted >0){
-                    System.out.println("Inserted");
-                    flag=1;
+                    if(rowsInserted >0){
+                        System.out.println("Inserted");
+                        flag=1;
+                    }
                 }
+
             }
+
+            if(flag == 0) System.out.println("Product not found ");
+
 
         }
+        catch (Exception e){
+            System.out.println(e);
+        }
 
-        if(flag == 0) System.out.println("Product not found ");
     }
 
-    public static  void orderHistory(ResultSet resultSet, String name) throws SQLException {
+    public    void orderHistory(ResultSet resultSet, String name)  {
 
-        System.out.println("UserName"+" "+"Product Id");
-        while (resultSet.next()){
-            if(resultSet.getString(2).equals(name)){
-                System.out.print(resultSet.getString(2) +" ");
-                System.out.print(resultSet.getInt(3));
-            }
+        try{
+            System.out.println("UserName"+" "+"Product Id");
+            while (resultSet.next()){
+                if(resultSet.getString(2).equals(name)){
+                    System.out.print(resultSet.getString(2) +" ");
+                    System.out.print(resultSet.getInt(3));
+                }
 
 //             System.out.println(resultSet.getString(2)+" "+resultSet.getInt(3));
 
-            System.out.println();
+                System.out.println();
+            }
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
+
+
+
 }
